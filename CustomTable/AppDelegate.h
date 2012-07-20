f2347b69daa788f78f0c0dbaad445c845392889b
Punit Sindhwani <punitsindhwani@Punit-Sindhwanis-Mac-mini.local>
2012-07-20 15:23:32 +0530

    Initial Commit

---
+++ CustomTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomTable
+//
+//  Created by Punit Sindhwani on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
